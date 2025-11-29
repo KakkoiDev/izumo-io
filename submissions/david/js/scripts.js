379eb852ca492e4ca816360d5a8c0af0bbfc54a8
David Robel <david_robel@hotmail.com>
2025-11-30 01:47:57 +0900

    ollama fetch and basic CSS ok

--- submissions/david/js/scripts.js
+++ submissions/david/js/scripts.js
@@ -5,6 +5,40 @@ function getTxtValue() {
   //get value from TEXTarea
   const userTxtValue = userValue.value;
 
+  // Ollama serve send mesage
+   fetch('http://192.168.1.244:11434/api/generate', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          model: 'dolphin3:latest', // MODEL
+          prompt: userTxtValue,  //messege
+          stream: false,
+        }),
+      })
+      .then(response => response.json())
+      .then(data => {
+          console.log(data)
+
+          //ollama retur
+          const aiResponse = data.response
+
+          //HTML element for AI message
+          const aiDiv = document.createElement("DIV");
+          aiDiv.className = "aiDiv"
+
+          const pAi = document.createElement("p")
+          pAi.textContent = aiResponse
+
+          //put elements on screen
+          aiDiv.appendChild(pAi)
+          mainDiv.appendChild(aiDiv)
+
+        }) // response
+      .catch(error => console.error('Error:', error));
+
+
   // create a HTML elements with the value
   const userTxtDiv = document.createElement("DIV");
   userTxtDiv.className = "userTxtDiv";
@@ -23,3 +57,6 @@ function getTxtValue() {
   // cursor go back on text area
   userValue.focus();
 }
+
+window.getTxtValue = getTxtValue;
+
